9aa232084032a80f499908d53ba81501e829e189
lucahardonk <wmsi@wmsi.localdomain>
2025-12-04 19:14:21 +0100

    added launch file even for windows from wsl

--- ros2_ws/src/camera_pkg/setup.py
+++ ros2_ws/src/camera_pkg/setup.py
@@ -9,17 +9,24 @@ setup(
     version='0.0.1',
     packages=[package_name],
     data_files=[
+        # Register package
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+
+        # package.xml
         ('share/' + package_name, ['package.xml']),
-        # Include the calibration YAML file
+
+        # Calibration YAML file
         ('share/' + package_name, ['fisheye_camera.yaml']),
+
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='your_name',
     maintainer_email='your_email@example.com',
-    description='Camera calibration and undistortion package',
+    description='Camera package with calibration, visualization, and filtering nodes.',
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
@@ -28,6 +35,7 @@ setup(
             'gaussian_blur_node = camera_pkg.gaussian_blur:main',
             'camera_visualizer_node = camera_pkg.camera_visualizer_node:main',
             'canny_edge_node = camera_pkg.canny_node:main',
+
         ],
     },
 )
