e8df81cae99eb749742ba924b0aca68ae6e6f0fd
lucahardonk <wmsi@wmsi.localdomain>
2025-12-11 12:37:20 +0100

    tested and working so far

--- ros2_ws/src/camera_pkg/setup.py
+++ ros2_ws/src/camera_pkg/setup.py
@@ -31,15 +31,20 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'first_camera_node_linux_native = camera_pkg.first_camera_node_linux_native:main',
             'camera_calibrated_node = camera_pkg.camera_calibrated_node:main',
             'gaussian_blur_node = camera_pkg.gaussian_blur:main',
-            'camera_visualizer_node = camera_pkg.camera_visualizer_node:main',
             'canny_edge_node = camera_pkg.canny_node:main',
             'background_removal_node = camera_pkg.background_removal_node:main',
             'grey_scaled_node = camera_pkg.grey_scaled_node:main',
             'otsu_node = camera_pkg.otsu_thresholding:main',
             'morphological_closure_node = camera_pkg.morphological_closure:main',
             'contour_detection_node = camera_pkg.contour_detection_node:main',
+            'min_rect_area_node = camera_pkg.min_rect_area:main',
+            'physical_features_node = camera_pkg.physical_features_extraction:main',
+            
+            'camera_visualizer_node = camera_pkg.camera_visualizer_node:main',
+            
             
 
         ],
